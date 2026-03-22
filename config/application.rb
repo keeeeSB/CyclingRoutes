0c7a3cd308f52c0da7982079b6de1e08374baa43
KeeeeSB <kfp.a@icloud.com>
2026-03-22 13:40:54 +0900

    rspecのインストール

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module CyclingRoutes
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.0
 
-    config.time_zone = 'Toyko'
+    config.time_zone = 'Tokyo'
     config.i18n.default_locale = :ja
 
     config.generators do |g|
